15f855d007f2d4a9ec01520d2e2f6e217f6647fd
Alexis Montes <alexismontes@Alexiss-MacBook-Pro.local>
2020-01-23 01:50:58 -0500

    added new icons and greeting typewriter effect

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -42,6 +42,8 @@
 
 })(jQuery); // End of use strict
 
+
+
 // Disable Google Maps scrolling
 // See http://stackoverflow.com/a/25904582/1607849
 // Disable scroll zooming and bind back the click event
@@ -62,3 +64,7 @@ var onMapClickHandler = function(event) {
 }
 // Enable map zooming with mouse scroll when the user clicks the map
 $('.map').on('click', onMapClickHandler);
+
+const element = document.getElementById("mainIntro");
+
+
---
+++ js/typeEffect.js
@@ -0,0 +1,6 @@
+new TypeIt('#mainIntro', {
+    strings: ["console.log(greeting);", "Hello World!"],
+    speed: 100,
+    breakLines: false,
+    waitUntilVisible: true
+  }).go();
